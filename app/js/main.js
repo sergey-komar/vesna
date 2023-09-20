6dc1b661d8f5175561081812d0fd0ddb4bd8dec9
sergey-komar <kursev81@yandex.ru>
2023-09-20 21:14:57 +0300

    home-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
 
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+    const signupActive = document.querySelector('.signup-block__info-active');
+    const signupitem = document.querySelectorAll('.signup-block__info-item');
+
+    signupActive.addEventListener('click', () => {
+        signupitem.forEach(item => {
+            item.classList.toggle('opacity');
+        })
+    })
+})
